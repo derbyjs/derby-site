f4b7a551707ebcfc25331389dfa33544991a66e0
Nate Smith <nate@nateps.com>
2018-07-20 00:30:13 -0700

    fix derby code block syntax highlighting

--- src/server/derbyLanguage.js
+++ src/server/derbyLanguage.js
@@ -6,30 +6,108 @@ Description: Matcher for DerbyJS HTML Templates
 */
 
 module.exports = function(hljs) {
-  var EXPRESSION_KEYWORDS = 'if unless else each with on view unbound bound unescaped as';
+  var TEMPLATE_KEYWORDS = 'if unless else each with on view unbound bound unescaped as';
+  var TEMPLATE = {
+    className: 'template',
+    begin: '{{', end: '}}',
+    keywords: TEMPLATE_KEYWORDS
+  };
+  var XML_IDENT_RE = '[A-Za-z0-9\\._:-]+';
+  var TAG_INTERNALS = {
+    endsWithParent: true,
+    illegal: /</,
+    relevance: 0,
+    contains: [
+      {
+        className: 'attr',
+        begin: XML_IDENT_RE,
+        relevance: 0
+      },
+      {
+        begin: /=\s*/,
+        relevance: 0,
+        contains: [
+          {
+            className: 'string',
+            endsParent: true,
+            variants: [
+              {begin: /"/, end: /"/},
+              {begin: /'/, end: /'/},
+              {begin: /[^\s"'=<>`]+/}
+            ],
+            contains: [
+              TEMPLATE
+            ]
+          }
+        ]
+      }
+    ]
+  };
   return {
     aliases: [],
     case_insensitive: true,
-    subLanguage: 'xml',
-    subLanguageMode: 'continuous',
     contains: [
-    {
-      className: 'expression',
-      begin: '{{', end: '}}',
-      keywords: EXPRESSION_KEYWORDS
-    },
-    // this catches the cases for <span class="{{myClass}}">
-    {
-      className: 'expression',
-      begin: '"{{', end: '}}"',
-      keywords: EXPRESSION_KEYWORDS
-    },
-    // this catches an edge case for <span class="color:{{color}}">
-    {
-      className: 'expression',
-      begin: '"', end: '}}"',
-      keywords: EXPRESSION_KEYWORDS
-    }
+      {
+        className: 'meta',
+        begin: '<!DOCTYPE', end: '>',
+        relevance: 10,
+        contains: [{begin: '\\[', end: '\\]'}]
+      },
+      hljs.COMMENT(
+        '<!--',
+        '-->',
+        {
+          relevance: 10
+        }
+      ),
+      {
+        begin: '<\\!\\[CDATA\\[', end: '\\]\\]>',
+        relevance: 10
+      },
+      TEMPLATE,
+      {
+        className: 'tag',
+        /*
+        The lookahead pattern (?=...) ensures that 'begin' only matches
+        '<style' as a single word, followed by a whitespace or an
+        ending braket. The '$' is needed for the lexeme to be recognized
+        by hljs.subMode() that tests lexemes outside the stream.
+        */
+        begin: '<style(?=\\s|>|$)', end: '>',
+        keywords: {name: 'style'},
+        contains: [TAG_INTERNALS],
+        starts: {
+          end: '</style>', returnEnd: true,
+          subLanguage: ['css', 'xml']
+        }
+      },
+      {
+        className: 'tag',
+        // See the comment in the <style tag about the lookahead pattern
+        begin: '<script(?=\\s|>|$)', end: '>',
+        keywords: {name: 'script'},
+        contains: [TAG_INTERNALS],
+        starts: {
+          end: '\<\/script\>', returnEnd: true,
+          subLanguage: ['javascript', 'xml']
+        }
+      },
+      {
+        className: 'meta',
+        variants: [
+          {begin: /<\?\w+/, end: /\?>/}
+        ]
+      },
+      {
+        className: 'tag',
+        begin: '</?', end: '/?>',
+        contains: [
+          {
+            className: 'name', begin: /[^\/><\s]+/, relevance: 0
+          },
+          TAG_INTERNALS
+        ]
+      }
     ]
   };
 };
